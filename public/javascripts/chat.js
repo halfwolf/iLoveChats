7e5d6cdc9f7c1e75769390e93db9b9e51740725b
Colin Day <c.day108@gmail.com>
2014-10-27 15:43:35 -0400

    add multiple user support and nickname changing

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,3 +5,7 @@ function Chat(socket) {
 Chat.prototype.sendMessage = function(message) {
   this.socket.emit('message', message)
 }
+
+Chat.prototype.sendNameChange = function(name) {
+  this.socket.emit('nicknameChangeRequest', name)
+}
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -11,17 +11,41 @@ var sendMessageToChat = function(msg) {
   newChat.sendMessage(msg);
 }
 
+var sendNameChange = function(msg) {
+  var name = msg.slice(6);
+  newChat.sendNameChange(name);
+}
+
+var evaluateParse = function(msg) {
+  if (msg.slice(0, 5) === "/nick") {
+    sendNameChange(msg);
+  } else {
+    insertIntoHTML("invalid command");
+  }
+}
+
 var insertIntoHTML = function(msg) {
   $('div.messages').append($("<li></li>").text(msg))
 }
 
-$(document).ready(function() {
 
+$(document).ready(function() {
+  socket.on("this", function(mesg) {
+    var msg = (mesg.name + " says "+ mesg.message)
+    insertIntoHTML(msg)
+  });
+
+  socket.on("nicknameChangeResult", function(mesg) {
+    insertIntoHTML(mesg.message)
+  });
 
   $('form.message-form').bind("submit", function(event){
     event.preventDefault();
     var msg = messageToInput($('input'))
-    sendMessageToChat(msg);
-    insertIntoHTML(msg);
+    if (msg.slice(0, 1) === "/") {
+      evaluateParse(msg)
+    } else {
+      sendMessageToChat(msg);
+    }
   })
 });
